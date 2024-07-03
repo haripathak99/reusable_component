2f9ff806a40aab4e1ab15aca27bf02b859661be7
haripathak99 <haripathak.naxa@gmail.com>
2024-07-03 10:10:17 +0545

    docs: documentation testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-
-
-import './App.css'
-import Button from './components/Button'
-import Image from './components/Image'
+import "./App.css";
+import Button from "./components/Button";
+import Image from "./components/Image";
 
 function App() {
-  
-
   return (
     <>
-    <h1>Reusable Component</h1>
-      <Button label="Click me" color="blue" onClick={()=> console.log("Button clicked")} />
-      <Image source='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREoRGyXmHy_6aIgXYqWHdOT3KjfmnuSyxypw&s'
-      width={300} height={200} alt='Girl with camera' />
+      {/* test documentation */}
+      <h1>Reusable Component</h1>
+      <Button
+        label="Click me"
+        color="blue"
+        onClick={() => console.log("Button clicked")}
+      />
+      <Image
+        source="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREoRGyXmHy_6aIgXYqWHdOT3KjfmnuSyxypw&s"
+        width={300}
+        height={200}
+        alt="Girl with camera"
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
